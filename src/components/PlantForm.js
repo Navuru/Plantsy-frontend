e97c5b846f63b1259d69d24a25e15a93a4cc873d
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-07 10:49:02 +0300

    Add submit functionality to plant form

--- src/components/PlantForm.js
+++ src/components/PlantForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function PlantForm () {
+function PlantForm (onAddPlant) {
 
     const [formData,setFormData] = useState({
         name: "",
@@ -8,15 +8,47 @@ function PlantForm () {
         description: "",
     });
 
+    function handleChange(e) {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value,
+        });
+      }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const newPlant = {
+            ...formData,
+            likes: 0,
+        };
+        fetch("/plants",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPlant),
+        })
+        .then((r) => r.json())
+        .then((newPlant) => {
+            setFormData({
+                name: "",
+                image: "",
+                description: "",
+            });
+            onAddPlant(newPlant);
+        });
+    }
+
     return (
         <div className= "container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Add a plant!</h3>
                 <input
                 type="text"
                 name="name"
                 value={formData.name}
                 placeholder="Enter plant name..."
+                onChange={handleChange}
                 />
                 <br/> <br/>
                 <input
@@ -24,6 +56,7 @@ function PlantForm () {
                 name="image"
                 value={formData.image}
                 placeholder="Enter plant's image url..."
+                onChange={handleChange}
                 />
                  <br/> <br/>
                  <input
@@ -31,6 +64,7 @@ function PlantForm () {
                 name="description"
                 value={formData.description}
                 placeholder="Enter plant's description..."
+                onChange={handleChange}
                 />
                 <br/><br/>
                  <input
