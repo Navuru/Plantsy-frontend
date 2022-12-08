27346ea6cb38e0091385ffb7221ffc8bb9e4c716
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-08 08:28:05 +0300

    Rename navbar to header

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div id="toy-header">
+      <img
+        src="./assests/plantsylogo"
+        alt="toy header"
+      />
+    </div>
+  );
+}
+
+export default Header;
