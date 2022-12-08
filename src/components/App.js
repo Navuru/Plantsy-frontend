1f08973572f5e32db2570c60ba87ea7932981a9e
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-08 05:19:16 +0300

    Add base router to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from 'react';
-// import {BrowserRouter} from "react-router-dom";
-import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
-// import { Route, Routes } from "react-router-dom";
-import Header from "./Header";
-// import SignUp from "./SignUp";
-import Login from "./Login"
-import PlantContainer from './PlantContainer';
-
+import BaseRouter from "../routes";
+import {BrowserRouter as BrowserRouter} from "react-router-dom";
 
 
 function App() {
   const [user, setUser] = useState(null);
 
+  // useEffect(() => {
+  //   //auto login
+  //   fetch("/me").then((r) => {
+  //     if (r.ok) {
+  //       r.json().then((user) => setUser(user));
+  //     }
+  //   });
+  // }, []);
+
+  // if (!user) return <Login onLogin={setUser}/>
   useEffect(() => {
-    //auto login
+    // auto-login
+    autologin();
+  }, []);
+  const autologin = () => {
     fetch("/me").then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
       }
     });
-  }, []);
-
-  if (!user) return <Login onLogin={setUser}/>
+  };
   
   return (
+    <>
     <Router>
-    <div>
-      <Routes>  
-
-      <Route exact path="/plants" element={<PlantContainer/>}></Route>
-    </Routes>
-    
-    </div>
-    <Header/>
+    <BaseRouter user={user} setUser={setUser} autologin={autologin} />
     </Router>
+    </>
     
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./login/Login.js";
-import Home from "./home/Home.js";
-import Contact from "./contact/Contact";
-import Review from "./form/Review.js";
-import Signup from "./signup/SignUp.js";
-// import EditPropertyForm from "./components/AddPropertyForm ";
+import Login from "./components/Login.js";
+// import Plants from "./Plant.js";
+import Signup from "./components/SignUp.js";
+import Header from "./components/Header";
+
+
 const BaseRouter = ({setUser,user, autologin}) => (
   <div>
     <Routes>
-      <Route path="/home" element={<Home user={user} setUser={setUser} />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/review" element={<Review />} />
+      <Route path="/home" element={<Header user={user} setUser={setUser} />} />
       <Route path="/signup" element={<Signup autologin={autologin}/>} />
       <Route path="/" element={<Login autologin={autologin} />} />
 
