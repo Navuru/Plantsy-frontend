4e90819d49a3d7c07862bdcc1c3c78a0d95fd71f
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-12 06:29:48 +0300

    Update fetch links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar ({user, setUser}) {
   const navigate = useNavigate();
 
     function handleLogoutClick() {
-        fetch("plantsy-production-7d90.up.railway.app/plants/logout", { method: "DELETE" }).then((r) => {
+        fetch("plantsy-production-7d90.up.railway.app/logout", { method: "DELETE" }).then((r) => {
           if (r.ok) {
             setUser(null);
             navigate("/")
